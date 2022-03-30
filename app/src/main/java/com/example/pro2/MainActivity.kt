ca03c18938a4f84a83ad5f737118c9dd62612de1
Santiri2mil <santiago2mil@gmail.com>
2022-03-30 15:45:08 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pro2/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.pro2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+private val TAG="MainActivity"
+private val TEXT="TEXT_CONTENT"
+class MainActivity : AppCompatActivity() {
+    private var contenido: EditText?=null
+    private var titulo: EditText?=null
+     var conAux: String=""
+     var titAux: String=""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val guardar: Button =findViewById<Button>(R.id.button)
+        val borrar: Button =findViewById<Button>(R.id.button2)
+
+        contenido=findViewById<EditText>(R.id.contenido)
+        titulo=findViewById<EditText>(R.id.nota)
+
+
+        guardar?.setOnClickListener{GuardarNota()}
+        borrar?.setOnClickListener{}
+
+    }
+
+     fun GuardarNota(){
+        conAux=contenido?.text.toString()
+        titAux=titulo?.text.toString()
+
+    }
+    fun BorrarNota(){
+        contenido?.setText("")
+        titulo?.setText("")
+    }
+    fun setContenido(): String {//entrega el contenido de la nota
+        return conAux
+    }
+    fun setTitulo(): String{//entrega el titulo de la nota
+        return  titAux
+    }
+
+    //Metodos pre hechos
+    override fun onStart() {
+        Log.d(TAG,"onStart")
+        super.onStart()
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG,"onRestoreInstanceState")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+    override fun onResume(){
+        Log.d(TAG,"onResume")
+        super.onResume()
+    }
+    override fun onPause() {
+        Log.d(TAG,"onPause")
+        super.onPause()
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG,"onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT,contenido?.text.toString())
+    }
+    override fun onStop() {
+        Log.d(TAG,"onStop")
+        super.onStop()
+    }
+    override fun onRestart() {
+        Log.d(TAG,"onRestart")
+        super.onRestart()
+    }
+    override fun onDestroy() {
+        Log.d(TAG,"onDestroy")
+        super.onDestroy()
+    }
+}
